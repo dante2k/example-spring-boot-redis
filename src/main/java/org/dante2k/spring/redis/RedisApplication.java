d924f9a267c18efaafe6f421c9d0e54fafdac041
Eunjung Kim <dante2k@wholedoc.net>
2018-03-21 07:43:33 +0900

    Remove @EnableAutoConfiguration

--- src/main/java/org/dante2k/spring/redis/RedisApplication.java
+++ src/main/java/org/dante2k/spring/redis/RedisApplication.java
@@ -2,17 +2,12 @@ package org.dante2k.spring.redis;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
-import javax.annotation.PostConstruct;
-
 @Slf4j
-@EnableAutoConfiguration
 @SpringBootApplication
 public class RedisApplication {
 
